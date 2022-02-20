7b2a1fa96ef748a9f258a53eb9c518a027b7efe1
coders-kitchen <peter@coders-kitchen.com>
2022-02-20 12:33:03 +0100

    add missing dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,8 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.5.0"
+    kotlin("jvm") version "1.6.10"
+    application
 }
 
 group = "com.coderskitchen"
@@ -13,9 +14,12 @@ repositories {
 
 dependencies {
     implementation("org.hibernate.validator:hibernate-validator:7.0.2.Final")
+    implementation("org.glassfish:jakarta.el:4.0.1")
     testImplementation(kotlin("test-junit5"))
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.6.0")
+    testImplementation("org.assertj:assertj-core:3.22.0")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
+    testImplementation("org.junit.jupiter:junit-jupiter-params:5.8.1")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
 }
 
 tasks.test {
@@ -24,4 +28,8 @@ tasks.test {
 
 tasks.withType<KotlinCompile>() {
     kotlinOptions.jvmTarget = "11"
+}
+
+application {
+    mainClass.set("com.coderskitchen.jsr380examples.ApplicationKt")
 }
