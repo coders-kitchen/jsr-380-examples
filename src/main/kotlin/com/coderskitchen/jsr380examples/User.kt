255413a19d5f5f1d3fd627bbb3a311b50efc5412
coders-kitchen <peter@coders-kitchen.com>
2022-02-20 12:32:49 +0100

    adapt the user model

--- src/main/kotlin/com/coderskitchen/jsr380examples/User.kt
+++ src/main/kotlin/com/coderskitchen/jsr380examples/User.kt
@@ -4,13 +4,14 @@ import jakarta.validation.constraints.Max
 import jakarta.validation.constraints.Min
 import jakarta.validation.constraints.NotNull
 import jakarta.validation.constraints.Size
+import org.hibernate.validator.constraints.Range
 
 data class User(
-    @NotNull(message = "Name cannot be null")
-    val name: String,
-    @Min(value = 18, message = "Age should not be less than 18")
-    @Max(value = 150, message = "Age should not be greater than 150")
+    @get:Size(min=5, max= 30, message = "Name must be between 5 and 30 characters")
+    @get:NotNull(message = "Name cannot be null")
+    val name: String?,
+    @get:Range(min = 18, max  = 150, message = "Age must be between 18 and 150")
     val age: Int,
-    @Size(min=10, max=200, message = "About Me must be between 10 and 200 characters")
+    @get:Size(min=10, max=200, message = "About Me must be between 10 and 200 characters")
     val aboutMe: String
 )
